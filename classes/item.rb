28d64dc6f102cdd90fe418d22bb0b563887e261d
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-04 06:30:16 +0100

    create and initialize item class

--- classes/item.rb
+++ classes/item.rb
@@ -1,3 +1,12 @@
+require 'date'
+
 class Item
+attr_accessor :genre, :author, :source, :label
+
+def initialize(publish_date, archived)
+    @id = id
+    @publish_date = publish_date
+    @archived = archived
+end
 
 end
