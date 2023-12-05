f4ae993dd400058332eb06a1298a7d17411d5eac
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-05 22:52:05 +0100

    Create and edit item class

--- classes/item.rb
+++ classes/item.rb
@@ -1,12 +1,27 @@
 require 'date'
 
 class Item
-attr_accessor :genre, :author, :source, :label
+attr_accessor :genre, :author, :source, :label, :publish_date, :archived
+attr_reader :id
 
 def initialize(publish_date, archived)
-    @id = id
-    @publish_date = publish_date
+    @id = rand(1..1000)
+    @publish_date = Date.parse(publish_date) if publish_date
     @archived = archived
 end
 
+def can_be_archived
+    return false unless publish_date
+    current_date = Date.today.year
+    to_be_archived = current_date - @publish_date.year
+    to_be_archived > 10
 end
+
+def move_to_archive
+    @archived = can_be_archived?
+end
+
+end
+
+# item1 = Item.new('2023-01-12', true)
+# puts "#{item1.can_be_archived}"
