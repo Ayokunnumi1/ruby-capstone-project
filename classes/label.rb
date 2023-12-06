2f3cb896d603dd5ecfee1718f75494b5ae5f3b30
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-06 07:25:06 +0100

    Edit and activate label class

--- classes/label.rb
+++ classes/label.rb
@@ -1,7 +1,9 @@
-class label
+class Label
+    attr_accessor :title, :color
+    attr_reader :id
 
     def initialize(title, color)
-        @id = id
+        @id = rand(1..1000)
         @title = title
         @color = color
         @items = []
