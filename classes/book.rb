d6955ad05bd467f46e017681021a818b3b9f0c2c
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-06 07:24:26 +0100

    Include end on initialize method

--- classes/book.rb
+++ classes/book.rb
@@ -9,6 +9,7 @@ class Book < Item
     @id = rand(1..1000)
     @publisher = publisher
     @cover_state = cover_state
+    end
     
     def can_be_archived
         super() || cover_state == 'bad'
